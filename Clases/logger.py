3733cd6fe21b342806cb8eb3ece2ad0427fcd15a
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-03-21 22:53:56 +0000

    creo la clase Test y la variables palabra y n

---
+++ Clases/logger.py
@@ -0,0 +1,27 @@
+
+Class Test():
+  def llamada(self,p):
+    self.palabra = p
+    for i in range (1,n):
+      
+
+
+
+
+palabra=input(string("Introduce la palabra: "))
+n=input(int("¿Cuántas veces desea repetil la palabra: "))
+
+#test = Test() 
+#for i in range(1, 6): 
+ #  if i == 1: 
+  #     test.llamada("Primera llamada") 
+   #else: 
+    #   test.llamada("{}ª llamada".format(string)) 
+#$> cat log.txt 
+#--Start log-- 
+#Primera llamada 
+#2a llamada 
+#3a llamada 
+#4a llamada 
+#5a llamada 
+#--End log: 5 log(s)-- 
