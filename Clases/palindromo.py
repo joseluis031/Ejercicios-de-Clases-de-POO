4d6c400b94b0cbe22fb3f2e0822a0871b8394941
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-03-21 21:59:15 +0000

    modifico el código a POO

--- Clases/palindromo.py
+++ Clases/palindromo.py
@@ -1,21 +1,33 @@
 palabra = input("Introduce la palabra que quieras comprobar si es un palindromo")
-class Palindromo:
-    def iniciar (self,)
+class palindromo:
+    def iniciar (self,p):
+      self.cadena = p
+
+    def correcto(self):
+      if len(palabra) < 1:
+        print ( self.cadena ," es un palindromo!")
+      else:
+         if palabra[0] == palabra[-1]:
+           palindromo(palabra[1:-1])
+           else:
+                print(self.cadena, "no es un palindromo")
 
 
 
+palindromo1 = palindromo()
+palindromo1.iniciar(palabra)
 
 
 
 
 
   
-    def palindromo(palabra):
-        if len(palabra) < 1:
-            print(palabra, "palindromo!!")
-        else:
-            if palabra[0] == palabra[-1]:
-                palindromo(palabra[1:-1])
-            else:
-                print(palabra, "no es un palindromo")
-    palindromo(palabra)
+    #def palindromo(palabra):
+     #   if len(palabra) < 1:
+      #      print(palabra, "palindromo!!")
+       # else:
+        #    if palabra[0] == palabra[-1]:
+         #       palindromo(palabra[1:-1])
+          #  else:
+           #     print(palabra, "no es un palindromo")
+  #  palindromo(palabra)
