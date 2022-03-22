4f92a408514adb8fca9ae41af6e0fb44f36bae5b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-22 09:30:25 +0100

    hago el main

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from Clases.palindromo import *
-
 if __name__ == "__main__":
+    main = int(input("Que ejercicio deseas realizar(1,2,3 o 4):"))
+    if main == 1:
+        from Clases.palindromo import *
+        print("Ejercicio palindromo: ")
+        palindromo.correcto()
+    elif main == 2:
+        from Clases.palinstancia import *
+        print("2º ejercicio palindromo: ")
+        palindromo2.correcto2()
+    elif main == 3:
+        from Clases.puzzle import *
+        print("Ejercicio puzzle: ")
+        A.y()
+    elif main == 4:
+        from Clases.logger import *
+        print("Ejercicio logger: ")
+        Test.llamada()
+    else:
+        print("Ese número no es valido")
     
