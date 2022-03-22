55b105d4bfcd5936ed49bcbc47caf000356e6ed2
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-03-22 09:30:51 +0100

    aclaracion

--- Clases/palinstancia.py
+++ Clases/palinstancia.py
@@ -1,13 +1,13 @@
 palabra = input("Introduce la palabra que quieras comprobar si es un palindromo: ")
-class palindromo:
+class palindromo2:
     def __init__(self,palabra):
       self.palabra = palabra
-    def correcto(self):
+    def correcto2(self):
       self.s = palabra[::-1]
       if self.s ==self.palabra:
         print(palabra.upper(),"True")
       else: 
         print(palabra.upper(),"False")
 
-palindromo1 = palindromo(palabra)
-print(palindromo1.correcto())
+palindromo1 = palindromo2(palabra)
+print(palindromo1.correcto2())
